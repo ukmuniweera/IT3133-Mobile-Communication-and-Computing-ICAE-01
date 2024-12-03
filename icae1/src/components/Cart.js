282118a2c5c12a07473da0b37e6c97ab35e5bded
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2024-12-03 23:26:39 +0530

    Update Cart.js

--- icae1/src/components/Cart.js
+++ icae1/src/components/Cart.js
@@ -1,25 +1,33 @@
-export default function Cart() {
-    
+import React from 'react';
+
+export default function Cart({ cartItems }) {
+    const total = cartItems.reduce((acc, item) => acc + item.price * item.qty, 0);
+
     return (
         <div className="table-container">
             <h4 className="card-title">Cart</h4>
             <table>
                 <thead>
-
-                    <td>Product</td>
-                    <td>QTY</td>
-                    <td>Price</td>
-
+                    <tr>
+                        <td>Product</td>
+                        <td>QTY</td>
+                        <td>Price</td>
+                    </tr>
                 </thead>
-                {
-                    
-                }
-                <tr>
-                    <td >Grand Total : </td>
-                    <td colSpan={2}>Grand Total</td>
-                </tr>
+                <tbody>
+                    {cartItems.map(item => (
+                        <tr key={item.id}>
+                            <td>{item.name}</td>
+                            <td>{item.qty}</td>
+                            <td>{item.price * item.qty}</td>
+                        </tr>
+                    ))}
+                    <tr>
+                        <td>Grand Total:</td>
+                        <td colSpan={2}>{total}</td>
+                    </tr>
+                </tbody>
             </table>
-           
         </div>
     );
 }
